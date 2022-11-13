a15633411c7dedddec87e4127ec50ad50145dd12
Arthur_Ch <56088401+ArthurChains@users.noreply.github.com>
2022-11-13 16:15:02 +0100

    feat: image_reader bindings

--- cpp/pybind/plane_extraction/plane_extraction.cpp
+++ cpp/pybind/plane_extraction/plane_extraction.cpp
@@ -1,11 +1,13 @@
 #include "plane_extraction.h"
 #include <deplex/plane_extraction.h>
+#include <deplex/image_reader.hpp>
 
 namespace deplex {
 void pybind_plane_extraction(py::module& m) {
   py::module m_plane_extraction = m.def_submodule(
       "plane_extraction", "Module with Plane Extraction algorithm");
 
+  pybind_read_image(m_plane_extraction);
   pybind_config(m_plane_extraction);
   pybind_extractor(m_plane_extraction);
 }
@@ -13,6 +15,13 @@ void pybind_plane_extraction(py::module& m) {
 void pybind_config(py::module& m) {
   py::class_<deplex::config::Config>(m, "Config")
       .def(py::init<std::string>(), py::arg("path"));
+}
+
+void pybind_read_image(py::module& m) {
+  m.def("read_image",
+        [](const std::string& image_path, const std::string& intrinsics_path) {
+          return deplex::reader::readImage(image_path, intrinsics_path);
+        });
 }
 
 void pybind_extractor(py::module& m) {
--- cpp/pybind/plane_extraction/plane_extraction.h
+++ cpp/pybind/plane_extraction/plane_extraction.h
@@ -1,10 +1,12 @@
 #pragma once
 #include <pybind11/pybind11.h>
+#include <pybind11/eigen.h>
 
 namespace py = pybind11;
 
 namespace deplex{
 void pybind_plane_extraction(py::module& m);
 void pybind_config(py::module& m);
+void pybind_read_image(py::module& m);
 void pybind_extractor(py::module& m);
 }
