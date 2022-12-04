9b5b301e09e29018acb77fc5918e2335d8c61bbf
Arthur_Ch <56088401+ArthurChains@users.noreply.github.com>
2022-12-04 15:40:03 +0100

    feat: remove config field in cellSegment

--- cpp/deplex/src/deplex/cell_segment.h
+++ cpp/deplex/src/deplex/cell_segment.h
@@ -20,17 +20,19 @@ class CellSegment {
 
  private:
   CellSegmentStat stats_;
-  config::Config const* const config_;
   bool is_planar_;
 
-  bool isValidPoints(Eigen::MatrixXf const& cell_points) const;
+  bool hasValidPoints(Eigen::MatrixXf const& cell_points, size_t valid_pts_threshold) const;
 
-  bool isDepthContinuous(Eigen::MatrixXf const& cell_points) const;
+  bool isDepthContinuous(Eigen::MatrixXf const& cell_points, int32_t cell_width, int32_t cell_height,
+                         float depth_disc_threshold, int32_t max_number_depth_disc) const;
 
-  bool isHorizontalContinuous(Eigen::MatrixXf const& cell_z) const;
+  bool isHorizontalContinuous(Eigen::MatrixXf const& cell_z, float depth_disc_threshold,
+                              int32_t max_number_depth_disc) const;
 
-  bool isVerticalContinuous(Eigen::MatrixXf const& cell_z) const;
+  bool isVerticalContinuous(Eigen::MatrixXf const& cell_z, float depth_disc_threshold,
+                            int32_t max_number_depth_disc) const;
 
-  bool isFittingMSE() const;
+  bool hasSmallPlaneError(float depth_sigma_coeff, float depth_sigma_margin) const;
 };
 }  // namespace deplex
