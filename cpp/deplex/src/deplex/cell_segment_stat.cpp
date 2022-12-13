f0cf4b383ec7032508a7a0c3c5f816fd058578be
Arthur_Ch <56088401+ArthurChains@users.noreply.github.com>
2022-12-13 18:30:16 +0100

    fix: fitPlane ensure eigenvalues order

--- cpp/deplex/src/deplex/cell_segment_stat.cpp
+++ cpp/deplex/src/deplex/cell_segment_stat.cpp
@@ -3,6 +3,7 @@
 #include <limits>
 
 #include <Eigen/Eigenvalues>
+#include <iostream>
 
 namespace deplex {
 CellSegmentStat::CellSegmentStat() : nr_pts_(0), mse_(std::numeric_limits<float>::max()), score_(0) {}
@@ -36,14 +37,21 @@ float CellSegmentStat::getD() const { return d_; };
 void CellSegmentStat::fitPlane() {
   Eigen::Matrix3f cov = variance_ - coord_sum_ * coord_sum_.transpose() / nr_pts_;
   Eigen::SelfAdjointEigenSolver<Eigen::Matrix3f> es(cov);
-  Eigen::VectorXf v = es.eigenvectors().col(0);
+
+  Eigen::Index max_es_ind = 0, min_es_ind = 0;
+  es.eigenvalues().maxCoeff(&max_es_ind);
+  es.eigenvalues().minCoeff(&min_es_ind);
+
+  Eigen::VectorXf v = es.eigenvectors().col(min_es_ind);
 
   d_ = -mean_.dot(v);
   // Enforce normal orientation
   normal_ = (d_ > 0 ? v : -v);
   d_ = (d_ > 0 ? d_ : -d_);
 
-  mse_ = es.eigenvalues()[2] / nr_pts_;
-  score_ = es.eigenvalues()[2] / es.eigenvalues()[0];
+  mse_ = es.eigenvalues()[min_es_ind] / nr_pts_;
+  // TODO: Debug cases when mse becomes negative (M3 of cov < 0)
+  if (mse_ < 0) mse_ = std::numeric_limits<float>::max();
+  score_ = es.eigenvalues()[max_es_ind] / (es.eigenvalues().sum());
 }
 }  // namespace deplex
