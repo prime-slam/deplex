9550abbd81513a998a1b206d4cefb735198078f0
Arthur_Ch <56088401+ArthurChains@users.noreply.github.com>
2022-12-14 11:55:13 +0100

    fix: segment stat double arithmetic

--- cpp/deplex/src/deplex/cell_segment_stat.cpp
+++ cpp/deplex/src/deplex/cell_segment_stat.cpp
@@ -3,16 +3,15 @@
 #include <limits>
 
 #include <Eigen/Eigenvalues>
-#include <iostream>
 
 namespace deplex {
 CellSegmentStat::CellSegmentStat() : nr_pts_(0), mse_(std::numeric_limits<float>::max()), score_(0) {}
 
-CellSegmentStat::CellSegmentStat(Eigen::MatrixXf const& points)
+CellSegmentStat::CellSegmentStat(Eigen::MatrixXd const& points)
     : nr_pts_(points.rows()),
       coord_sum_(points.colwise().sum()),
       variance_(points.transpose() * points),
-      mean_(coord_sum_ / nr_pts_) {
+      mean_(coord_sum_.cast<float>() / nr_pts_) {
   fitPlane();
 }
 
@@ -20,7 +19,7 @@ CellSegmentStat& CellSegmentStat::operator+=(CellSegmentStat const& other) {
   nr_pts_ += other.nr_pts_;
   coord_sum_ += other.coord_sum_;
   variance_ += other.variance_;
-  mean_ = coord_sum_ / nr_pts_;
+  mean_ = coord_sum_.cast<float>() / nr_pts_;
   return *this;
 }
 
@@ -35,23 +34,21 @@ float CellSegmentStat::getMSE() const { return mse_; };
 float CellSegmentStat::getD() const { return d_; };
 
 void CellSegmentStat::fitPlane() {
-  Eigen::Matrix3f cov = variance_ - coord_sum_ * coord_sum_.transpose() / nr_pts_;
-  Eigen::SelfAdjointEigenSolver<Eigen::Matrix3f> es(cov);
+  Eigen::Matrix3d cov = variance_ - coord_sum_ * coord_sum_.transpose() / nr_pts_;
+  Eigen::SelfAdjointEigenSolver<Eigen::Matrix3d> es(cov);
 
   Eigen::Index max_es_ind = 0, min_es_ind = 0;
   es.eigenvalues().maxCoeff(&max_es_ind);
   es.eigenvalues().minCoeff(&min_es_ind);
 
-  Eigen::VectorXf v = es.eigenvectors().col(min_es_ind);
+  Eigen::VectorXf v = es.eigenvectors().col(min_es_ind).cast<float>();
 
-  d_ = -mean_.dot(v);
+  d_ = static_cast<float>(-mean_.cast<float>().dot(v));
   // Enforce normal orientation
   normal_ = (d_ > 0 ? v : -v);
   d_ = (d_ > 0 ? d_ : -d_);
 
   mse_ = es.eigenvalues()[min_es_ind] / nr_pts_;
-  // TODO: Debug cases when mse becomes negative (M3 of cov < 0)
-  if (mse_ < 0) mse_ = std::numeric_limits<float>::max();
   score_ = es.eigenvalues()[max_es_ind] / (es.eigenvalues().sum());
 }
 }  // namespace deplex
