e04cdcd561692c184796af3301fa0296c52daf17
Arthur_Ch <56088401+ArthurChains@users.noreply.github.com>
2023-01-20 18:23:05 +0100

    feat: process_cloud example number of run argument

--- examples/process_cloud.cpp
+++ examples/process_cloud.cpp
@@ -5,14 +5,14 @@
 #include <filesystem>
 #include <iostream>
 
-int main() {
+int main(int argc, char* argv[]) {
   std::filesystem::path data_dir = std::filesystem::current_path().parent_path().parent_path() / "data";
   std::filesystem::path image_path = data_dir / "tum/points_1341848230.910894.csv";
   std::filesystem::path intrinsics_path = data_dir / "configs/TUM_fr3_long_val.K";
   std::filesystem::path config_path = data_dir / "configs/TUM_fr3_long_val.ini";
 
   constexpr int IMAGE_HEIGHT = 480, IMAGE_WIDTH = 640;
-  constexpr int NUMBER_OF_RUNS = 1;
+  int NUMBER_OF_RUNS = (argc > 1 ? std::stoi(argv[1]) : 1);
 
   deplex::config::Config config = deplex::config::Config(config_path.string());
 
