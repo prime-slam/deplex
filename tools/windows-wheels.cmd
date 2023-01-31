7dbbdab689df34dc78a436aa65ad7e6c054fcb24
Arthur_Ch <56088401+ArthurChains@users.noreply.github.com>
2023-01-31 13:56:19 +0100

    feat: windows script workaround

--- tools/windows-wheels.cmd
+++ tools/windows-wheels.cmd
@@ -13,7 +13,16 @@ SETLOCAL EnableDelayedExpansion
 
 SET plat="x64" || EXIT /B !ERRORLEVEL!
 
+:: Early check for build tools
+cmake --version || EXIT /B !ERRORLEVEL!
+
+pushd %~dp0
+cd ..
+
 for /D %%P in (C:\hostedtoolcache\windows\Python\3*) do CALL :build %plat% %%P\%~1\python.exe || popd && EXIT /B !ERRORLEVEL!
+
+popd
+EXIT /B 0
 
 :build
 cmake -S . -B build -G "Visual Studio 16 2019" -A "%~1" -DBUILD_PYTHON=ON -DBUILD_TESTS=OFF -DPYTHON_EXECUTABLE:FILEPATH=%~2  || EXIT /B !ERRORLEVEL!
