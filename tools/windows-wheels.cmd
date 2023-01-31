62e7399a4b0c1612b7ebf557e89d5f77bece7b20
Arthur_Ch <56088401+ArthurChains@users.noreply.github.com>
2023-01-31 14:22:20 +0100

    feat: python as matrix

--- tools/windows-wheels.cmd
+++ tools/windows-wheels.cmd
@@ -16,15 +16,9 @@ SET plat="x64" || EXIT /B !ERRORLEVEL!
 :: Early check for build tools
 cmake --version || EXIT /B !ERRORLEVEL!
 
-pushd %~dp0
-cd ..
+:: Install build package
+python -m pip install --user -q build || popd && EXIT /B !ERRORLEVEL!
 
-for /D %%P in (C:\hostedtoolcache\windows\Python\3*) do CALL :build %plat% %%P\%~1\python.exe || popd && EXIT /B !ERRORLEVEL!
-
-popd
-EXIT /B 0
-
-:build
-cmake -S . -B build -G "Visual Studio 16 2019" -A "%~1" -DBUILD_PYTHON=ON -DBUILD_TESTS=OFF -DPYTHON_EXECUTABLE:FILEPATH=%~2  || EXIT /B !ERRORLEVEL!
+:: Build wheel
+cmake -B build -G "Visual Studio 16 2019" -A %plat% -DBUILD_PYTHON=ON -DBUILD_TESTS=OFF || EXIT /B !ERRORLEVEL!
 cmake --build build --config Release --target build-wheel || EXIT /B !ERRORLEVEL!
-EXIT /B 0
