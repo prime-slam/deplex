c87d2c8fe5b5e658e1e0652d567386414d24ae31
Ilya Syresenkov <maybenotilya@bk.ru>
2023-07-20 19:59:48 +0300

    feat: add plane refinement stage

--- cpp/deplex/src/deplex/plane_extractor.cpp
+++ cpp/deplex/src/deplex/plane_extractor.cpp
@@ -29,6 +29,9 @@
 #include "cell_grid.h"
 #include "normals_histogram.h"
 
+#include <rtl/RANSAC.hpp>
+#include <rtl/Plane.hpp>
+
 #ifdef BENCHMARK_LOGGING
 namespace {
 template <typename T, typename Time>
@@ -106,6 +109,16 @@ class PlaneExtractor::Impl {
    * @returns Flatten array of labels of size [image_width x image_height]
    */
   Eigen::VectorXi toImageLabels(std::vector<int32_t> const& merge_labels);
+
+  /**
+   * Refines planes using RANSAC algorithm.
+   *
+   * @param pcd_array Points matrix [Nx3] of ORGANIZED point cloud
+   * i.e. points that refer to organized image structure.
+   * @param labels Flatten array of coarse planes labels
+   * @returns Flatten array of labels after plane refinement
+   */
+  Eigen::VectorXi refineLabels(Eigen::MatrixX3f const& pcd_array, Eigen::VectorXi const& labels);
 
   /**
    * Clean all used data for sufficient sequential image computing.
@@ -234,8 +247,25 @@ Eigen::VectorXi PlaneExtractor::Impl::process(Eigen::MatrixX3f const& pcd_array)
   std::ofstream of("dbg_3_labels.csv");
   of << labels.reshaped<Eigen::RowMajor>(image_height_, image_width_)
             .format(Eigen::IOFormat(Eigen::StreamPrecision, Eigen::DontAlignCols, ",", "\n"));
+  of.close();
 #endif
-  // 7. Cleanup
+  // 7. Refine planes
+  if (config_.ransac_refinement) {
+#ifdef BENCHMARK_LOGGING
+    auto time_refinement = std::chrono::high_resolution_clock::now();
+#endif
+    labels = refineLabels(pcd_array, labels);
+#ifdef BENCHMARK_LOGGING
+    std::clog << "[BenchmarkLogging] Labels refinement: "
+            << get_benchmark_time<decltype(std::chrono::microseconds())>(time_labels_creation) << '\n';
+#endif
+#ifdef DEBUG_DEPLEX
+    of.open("dbg_4_refined_labels.csv");
+    of << labels.reshaped<Eigen::RowMajor>(image_height_, image_width_)
+              .format(Eigen::IOFormat(Eigen::StreamPrecision, Eigen::DontAlignCols, ",", "\n"));
+#endif
+  }
+  // 8. Cleanup
   cleanArtifacts();
   return labels;
 }
@@ -437,6 +467,49 @@ Eigen::VectorXi PlaneExtractor::Impl::toImageLabels(std::vector<int32_t> const& 
   }
 
   return labels.reshaped<Eigen::RowMajor>();
+}
+
+Eigen::VectorXi PlaneExtractor::Impl::refineLabels(Eigen::MatrixX3f const& pcd_array, Eigen::VectorXi const& labels) {
+  Eigen::VectorXi refined_labels(labels);
+
+  std::vector<std::vector<int32_t>> labels_indeces(labels.maxCoeff());
+  for (int32_t i = 0; i < labels.size(); ++i) {
+    if (labels[i] != 0) {
+      labels_indeces[labels[i] - 1].push_back(i);
+    }
+  }
+
+  PlaneEstimator estimator;
+  RTL::PlaneRANSAC algorithm(&estimator);
+  algorithm.SetParamIteration(config_.ransac_max_iterations);
+  algorithm.SetParamTargetInliersRatio(config_.ransac_inliers_ratio);
+  algorithm.SetParamThreshold(config_.ransac_threshold);
+
+  for (int32_t label = 0; label < labels_indeces.size(); ++label) {
+    if (labels_indeces[label].size() == 0) {
+      continue;
+    }
+
+    Eigen::Vector4f plane_model(Eigen::Vector4f::Zero());
+    Eigen::MatrixX3f plane_pcd(labels_indeces[label].size(), pcd_array.cols());
+    for (int32_t row_id = 0; row_id < labels_indeces[label].size(); ++row_id) {
+      plane_pcd.row(row_id) = pcd_array.row(labels_indeces[label][row_id]);
+    }
+
+    algorithm.FindBest(plane_model, plane_pcd, labels_indeces[label].size(), plane_pcd.cols());
+    auto inliers = algorithm.FindInliers(plane_model, plane_pcd, labels_indeces[label].size());
+
+    int32_t current_inlier = 0;
+    for (int32_t i = 0; i < labels_indeces[label].size() && current_inlier < inliers.size(); ++i) {
+      if (inliers[current_inlier] != i) {
+        refined_labels[labels_indeces[label][i]] = 0;
+      } else {
+        ++current_inlier;
+      }
+    }
+  }
+
+  return refined_labels;
 }
 
 #ifdef DEBUG_DEPLEX
